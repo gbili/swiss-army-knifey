3380fdbaf8395568f0264f8d80ee3c15dc3aa84e
Guillermo Pages <mail@guillermo.at>
2021-12-03 00:20:21 +0200

    fix: add proper Logger interface

--- src/commonTypes.ts
+++ src/commonTypes.ts
@@ -1,3 +1,7 @@
-export type Logger = { log : (...params: any[]) => void, debug : (...params: any[]) => void };
+export type WithLog = { log : (...params: any[]) => void; }
+export type WithDebug = { debug : (...params: any[]) => void; }
+export type Logger = {}
+  & WithLog
+  & WithDebug
 export type CurryFuncWithDepsGen<D, F> = (deps: D) => F;
 export type NeedsLogger = { logger: Logger; }
