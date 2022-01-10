b71f3566c4eddc9355a2d04c9eb744c2ac24efde
Guillermo Pages <mail@guillermo.at>
2022-01-10 10:43:20 +0100

    feat: add request.get request.download functions

---
+++ src/utils/request.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+
+type Resolve<T> = T & { response: http.IncomingMessage; };
+
+export const download = async function (uri: string, dest: string, options?: https.RequestOptions) {
+  return new Promise(function (resolve: (res: Resolve<{ file: string; }>) => void, reject: (err: Error) => void){
+    const { get } = uri.match(/^https:\/\//g) !== null ? https : http;
+    const file = fs.createWriteStream(dest);
+    get(uri, options || {}, (resp) => {
+      resp.pipe(file)
+      file.on('finish', () => {
+        // The whole response has been received. Print out the result.
+        resolve({ file: dest, response: resp });
+      });
+    }).on("error", (err) => {
+      fs.unlink(dest, (err) => {
+        if (err) reject(err);
+      });
+      reject(err);
+    });
+  });
+}
+
+export const get = async function (uri: string, options?: https.RequestOptions) {
+  return new Promise(function (resolve: (res: Resolve<{ data: string; }>) => void, reject: (err: Error) => void){
+    const { get } = uri.match(/^https:\/\//g) !== null ? https : https;
+    get(uri, options || {}, (resp) => {
+      let data = '';
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        resolve({ data, response: resp });
+      });
+    }).on("error", (err) => {
+      reject(err);
+    });
+  });
+}
